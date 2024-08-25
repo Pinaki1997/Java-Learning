a5b6b672658241f08f922692ea0b836122b8e2e0
Pinaki1997 <pinakidash195@gmail.com>
2024-08-25 20:11:30 +0530

    call third party api using sprint boot

---
+++ src/main/java/com/example/ProductService/dtos/FakeStoreProductDto.java
@@ -0,0 +1,10 @@
+package com.example.ProductService.dtos;
+
+public class FakeStoreProductDto {
+    public int id;
+    public String title;
+    public double price;
+    public String description;
+    public String category;
+    public String image;
+}
---
+++ src/main/java/com/example/ProductService/models/Product.java
@@ -0,0 +1,16 @@
+package com.example.ProductService.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Product {
+    int id;
+    String name;
+    double price;
+    String description;
+    String category;
+    String image;
+
+}
